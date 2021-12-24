c712eb4998e05baeae16c4f3acf6f0bafbeac60c
mikaymgc <mk25f.y@gmail.com>
2021-12-25 03:32:02 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_info
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+    validates :user, foreign_key: true
+  end
 end
