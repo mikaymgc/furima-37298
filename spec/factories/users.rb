0d7a2c1836bf8b34cb982f678076d727150663f7
mikaymgc <mk25f.y@gmail.com>
2021-12-24 16:02:56 +0900

    単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email { "abc@com" }
+    password {"12345a"}
+    password_confirmation {"12345a"}
+    first_name { "山田" }
+    last_name { "太郎" }
+    first_name_kana { "ヤマダ" }
+    last_name_kana { "タロウ" }
+    user_birth_date {Faker::Date.birthday(min_age: 10, max_age: 90)}
+  end
+end
