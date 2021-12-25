9cbc3eb85113f42152ebe6f20105999c13431296
mikaymgc <mk25f.y@gmail.com>
2021-12-25 14:16:15 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
 
   with_options presence: true do
     validates :image
     validates :item_name
     validates :item_info
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_status_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
     validates :price
     validates :user, foreign_key: true
   end
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
